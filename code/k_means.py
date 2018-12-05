47f94419a2985124f03bfbc7fdb74d5bf55c3838
yuhao <yuhao@gitlab.com>
2018-12-05 21:55:56 +0800

    update work

--- code/k_means.py
+++ code/k_means.py
@@ -8,6 +8,7 @@ class KMeans:
         self.c_clusters = c_clusters
         self.centroids = list
         self.data = list
+        self.clustering_results = list
 
     def get_init_centroids(self):
         if self.c_clusters < 1:
@@ -21,3 +22,15 @@ class KMeans:
     def fit(self, data):
         self.data = data
         self.get_init_centroids()
+        dist_matrix = np.sqrt(
+            -2 * np.dot(data, self.centroids.T) + np.sum(np.square(self.centroids), axis=1) + np.transpose(
+                [np.sum(np.square(data), axis=1)]))
+        # calculate each point's new cluster
+        self.clustering_results = np.array([dist_matrix.argmin(axis=1)])
+
+        # calculate each cluster's new centroid
+        index_df = np.concatenate((self.clustering_results.T, self.data), axis=1)
+        index_df = pd.DataFrame(index_df).T
+        index_df.rename(columns={index_df.columns[0]: "index"}, inplace=True)
+        index_df.sort_values('index', inplace=True)
+        self.centroids = index_df.groupby('index').mean()
--- code/workspace.py
+++ code/workspace.py
@@ -1,5 +1,19 @@
 import numpy as np
 import random
+import pandas as pd
 
-a = np.array([[2, 3], [2, 3, 12], [2, 3, 3, 4], [3]]).tolist()
-print(random.sample(a, 2))
+a = np.array([[1, 1], [1, 1], [1, 1], [1, 1]])
+b = np.array([[2, 3], [2, 5], [4, 3], [2, 6]])
+
+dist_matrix = np.sqrt(
+    -2 * np.dot(a, b.T) + np.sum(np.square(b), axis=1) + np.transpose(
+        [np.sum(np.square(a), axis=1)]))
+# calculate each point's new cluster
+clustering_results = np.array([dist_matrix.argmin(axis=1)])
+
+index_df = np.concatenate((a.T, b), axis=1)
+index_df = pd.DataFrame(index_df)
+index_df.rename(columns={index_df.columns[0]: 'index'}, inplace=True)
+means = index_df.groupby('index').mean()
+
+print(means)
